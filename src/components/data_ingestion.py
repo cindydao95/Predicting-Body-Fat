a131bf5e903337a430908daebe09e84a832f7ecc
cindydao95 <cdao1295@gmail.com>
2023-03-31 10:04:11 -0500

    commit

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.json('artifacts',"data.csv")
+
+class 
--- src/logger.py
+++ src/logger.py
@@ -6,9 +6,10 @@ LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 log_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
 
-os.makedirs(logs_path,exist_ok =True)
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+os.makedirs(log_path,exist_ok =True)
+LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
 
 logging.bacsicConfig(filename = LOG_FILE_PATH,
                      format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
                      level = logging.INFO)
+
